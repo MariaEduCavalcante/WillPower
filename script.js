615860baf3744e76ee18f73619ffdf8e901f5acb
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2023-05-13 01:03:42 -0300

    home, footer e finalização imc

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ function calcIMC(){
     let peso =document.getElementById("peso").value;
     let altura = document.getElementById("altura").value ;
     let resultado = document.getElementById("resultado");
-    let genero = document.querySelector('input[name=genero]:checked').value
+    var genero = document.querySelector('input[name=genero]:checked').value;
     let imc = (peso / (altura * altura)).toFixed(2);
     let mensagem ="";
 
+    if(altura !=="" && peso !==""){
 
-    if(genero == f){
-        resultado.textContent ="Feminino";
+    if(genero == "f"){
+        if(imc < 19){
+            mensagem ="Abaixo do peso adequado";
+        } else if(imc < 23.9){
+            mensagem ="Peso adequado";
+        } else {
+            mensagem ="Peso acima do adequado";
+        }
     }
     else{
-        resultado.textContent ="Masculino";
+        if(imc < 20){
+            mensagem ="Abaixo do peso adequado";
+        } else if(imc < 25){
+            mensagem ="Peso adequado";
+        } else {
+            mensagem ="Peso acima do adequado";
+        }
     }
+    resultado.textContent =`Situação: ${mensagem}`;
+    alert( `Seu IMC é: ${imc}`)
 
+    }
+    else{
+        alert('Preencha todos os campos');
+    }
 }
 
 
